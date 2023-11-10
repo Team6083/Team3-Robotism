b351fefe4a10cfed34546b14141225922be74354
BrianHu0925 <j100376@my.cmsh.cyc.edu.tw>
2023-11-10 19:59:10 +0800

    [change] test

--- src/main/java/frc/robot/Base.java
+++ src/main/java/frc/robot/Base.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 
 public class Base {
     public static VictorSP R = new VictorSP(0);
-    public static VictorSP L = new VictorSP(9);
+    public static VictorSP L = new VictorSP(1);
     public static Joystick joy = new Joystick(0);
 
     public static void move() {
