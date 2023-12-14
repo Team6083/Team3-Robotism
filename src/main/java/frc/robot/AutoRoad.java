53ae0f969b4bb95f3d6f1a7c271f1b39fdc063aa
duckkk0323 <j100304@my.cmsh.cyc.edu.tw>
2023-12-14 19:21:20 +0800

    [Modify] AutoRoad add base

--- src/main/java/frc/robot/AutoRoad.java
+++ src/main/java/frc/robot/AutoRoad.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AutoRoad {
     public static SendableChooser<String> m_chooser;
     public static final String Auto = "Auto";
+    public static final String Default = "Default";
     public static  String m_autoSelected;
     public static Encoder enc = new Encoder(0, 1);
     public static AHRS gyro = new AHRS(SPI.Port.kMXP);
@@ -24,21 +25,30 @@ public class AutoRoad {
     }
 
     public static void chooser_setting() {
-        m_chooser.setDefaultOption("Auto", Auto);
+        m_chooser.setDefaultOption("Default", Default);
+        m_chooser.addOption("Auto", Auto);
         SmartDashboard.putData("Auto Route", m_chooser);
     }
 
-    public static void start() {
+    /*public static void start() {
         m_autoSelected = m_chooser.getSelected();
         timer.start();
     }
+    */
     
     public static void loop() {
         switch (m_autoSelected) {
             case Auto:
                 Auto();
                 break;
+            case Default:
+                Default();
         }
+    }
+
+    public static void Default(){
+        Base.R.set(0);
+        Base.L.set(0);
     }
 
     public static void Auto(){
