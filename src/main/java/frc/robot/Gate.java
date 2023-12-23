0ba035389e25223d8d9898c8998b84c4f668c1af
duckkk0323 <j100304@my.cmsh.cyc.edu.tw>
2023-12-23 09:22:21 +0800

    [add]GateL&R

--- src/main/java/frc/robot/Gate.java
+++ src/main/java/frc/robot/Gate.java
@@ -3,19 +3,27 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Servo;
 
 public class Gate {
-    public static Servo gate =new Servo(1);
+    public static Servo gateL = new Servo(1);
+    public static Servo gateR = new Servo(2);
     
     public static void Init(){
-        gate.set(0);
-        gate.setAngle(0);
+        gateL.set(0);
+        gateL.setAngle(0);
+        gateR.set(0);
+        gateR.setAngle(0);
     }
     
     
     public static void move(){if(Base.joy.getRawButton(0)==true){
-        gate.set(0.5);//A
-        gate.setAngle(90);
+        gateL.set(0.5);//A
+        gateL.setAngle(90);
+        gateR.set(0.5);
+        gateR.setAngle(90);
     }if(Base.joy.getRawButton(2)==true){
-        gate.set(0.5);//X
-        gate.setAngle(0);
+        gateL.set(0.5);//X
+        gateL.setAngle(0);
+        gateR.set(0.5);
+        gateR.setAngle(0);
+
     }
 }   }
